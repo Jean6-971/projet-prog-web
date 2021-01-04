f4b22a2ecd84ae119a9bbb4534c48a1ed00bef48
Jean Van Inghelandt <jean.vaninghelandt@outlook.fr>
2021-01-04 23:16:00 +0100

    Update snake.js

--- ressources/snake.js
+++ ressources/snake.js
@@ -1,29 +1,32 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var MR = Math.random;
+
 var rect_w = canvas.width/10;
 var rect_h = canvas.height/10;
-var x_dir = [-1, 0, 1, 0];
-var y_dir = [0, -1, 0, 1];
-var level = 100;
-var snake_color = "lime";
-var tn = [];
-var queue = [];
 var snkln = 3;
-var map = [];
-var MR = Math.random;
+var snake_color = "lime";
+
 var X = 5 + (MR() * (rect_w - 10))|0;
 var Y = 5 + (MR() * (rect_h - 10))|0;
+var x_dir = [-1, 0, 1, 0];
+var y_dir = [0, -1, 0, 1];
+
+var carte = [];
+var tmpdir = [];
 var direction = MR() * 3 | 0;
+var queue = [];
+
 var interval = 0;
 var score = 0;
 var i, dir;
 
 var score_actu = document.getElementById("scoractu");
-score_actu.innerHTML = "Votre score actuel : "+score;
+score_actu.innerHTML = "Votre score actuel : " + score;
 
 for (i = 0; i < rect_w; i++) {
-    map[i] = [];
+    carte[i] = [];
 }
 
 function random_apple() {
@@ -32,8 +35,8 @@ function random_apple() {
         x = MR() * rect_w|0;
         y = MR() * rect_h|0;
     }
-    while (map[x][y]);
-    map[x][y] = 1;
+    while (carte[x][y]);
+    carte[x][y] = 1;
     ctx.fillStyle = snake_color;
     var gradient = ctx.createLinearGradient(canvas.width * MR(), canvas.height * MR(), canvas.width * MR(), canvas.height * MR());
     gradient.addColorStop("0" ,"yellow");
@@ -45,14 +48,14 @@ function random_apple() {
 random_apple();
 
 function set_game_speed() {
-    if (tn.length) {
-        dir = tn.pop();
+    if (tmpdir.length) {
+        dir = tmpdir.pop();
         if ((dir % 2) !== (direction % 2)) {
             direction = dir;
         }
     }
-    if ((0 <= X && 0 <= Y && X < rect_w && Y < rect_h) && 2 !== map[X][Y]) {
-        if (1 === map[X][Y]) {
+    if ((0 <= X && 0 <= Y && X < rect_w && Y < rect_h) && 2 !== carte[X][Y]) {
+        if (1 === carte[X][Y]) {
             score += 1;
             var score_actu = document.getElementById("scoractu");
             score_actu.innerHTML = "Votre score actuel : "+score;
@@ -60,17 +63,17 @@ function set_game_speed() {
             snkln++;
         }
         ctx.fillRect(X * 10, Y * 10, 9, 9);
-        map[X][Y] = 2;
+        carte[X][Y] = 2;
         queue.unshift([X, Y]);
         X += x_dir[direction];
         Y += y_dir[direction];
         if (snkln < queue.length) {
             dir = queue.pop()
-            map[dir[0]][dir[1]] = 0;
+            carte[dir[0]][dir[1]] = 0;
             ctx.clearRect(dir[0] * 10, dir[1] * 10, 10, 10);
         }
     }
-    else if (!tn.length) {
+    else if (!tmpdir.length) {
         var show_score = document.getElementById("gameover");
         show_score.innerHTML = "<h1>Perdu !<br><br><u>Votre score : "+score+"<br><br><input type='button' value='Rejouer' onclick='window.location.reload();' />";
         document.getElementById("canvas").style.display = 'none';
@@ -80,7 +83,7 @@ function set_game_speed() {
     }
 }
 
-interval = window.setInterval(set_game_speed, level);
+interval = window.setInterval(set_game_speed, 100);
 
 document.onkeydown = function(e) {
     var code = 0;
@@ -103,7 +106,7 @@ document.onkeydown = function(e) {
             break;
     }
 
-    if (0 <= code && code < 4 && code !== tn[0]) {
-        tn.unshift(code);
+    if (0 <= code && code < 4 && code !== tmpdir[0]) {
+        tmpdir.unshift(code);
     }
 }
